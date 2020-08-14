b76e3057fa31dd20124482b97652a2dfa08c1019
Hunterprey <595129026@qq.com>
2020-08-14 17:37:25 +1000

    rewrite forecast

--- src/models/weather.js
+++ src/models/weather.js
@@ -17,57 +17,50 @@ const instance = axios.create({
 function filterDataByClass(allInfo) {
   const rawData = allInfo[0].data;
   const cityInfo = new city(rawData);
+  // console.log(cityInfo);
   const currentWeatherInfo = new currentWeather(rawData);
-  // const rawForecastData = allInfo[1].data;
-  //// console.log(rawForecastData);
+  //console.log(currentWeatherInfo);
+  const rawForecastData = allInfo[1].data.list;
+  //console.log(rawForecastData);
+  const forecastWeatherInfo = rawForecastData.map(
+    (i) => new forecastWeather(i)
+  );
+  //console.log(rawForecastData);
   // const forecastWeatherInfo = new forecastWeather(rawForecastData);
-  /// console.log(forecastWeatherInfo);
-  return { cityInfo, currentWeatherInfo };
+  // console.log(forecastWeatherInfo);
+  return { cityInfo, currentWeatherInfo, forecastWeatherInfo };
 }
 function getCurrentWeather() {
   // const reqCity = `${cityName},${countryCode}`;
-  return instance
-    .get("/weather", {
-      params: {
-        q: reqCity,
-        units: unit,
-        APPID: appid,
-      },
-    })
-    .then((res) => {
-      return res;
-    });
+  return instance.get("/weather", {
+    params: {
+      q: reqCity,
+      units: unit,
+      APPID: appid,
+    },
+  });
 }
 function getForecastWeather() {
   //api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
   //const reqCity = `${cityName},${countryCode}`;
   const cnt = 1;
-  instance
-    .get("/forecast", {
-      params: {
-        q: reqCity,
-        units: unit,
-        cnt: cnt,
-        APPID: appid,
-      },
-    })
-    .then((res) => {
-      //  const listtry = res.data.list[5]
-      const list = res.data.list;
-      let try1 = {};
-      try1 = list;
-      console.log(try1[0]);
-      const forecastWeatherInfo = new forecastWeather(try1[0]);
-      console.log(forecastWeatherInfo);
-      return try1;
-      //  console.log(list);
-    });
+  return instance.get("/forecast", {
+    params: {
+      q: reqCity,
+      units: unit,
+      cnt: cnt,
+      APPID: appid,
+    },
+  });
 }
 
 function getInfoByCityName() {
   Promise.all([getCurrentWeather(), getForecastWeather()]).then((res) => {
     const finalInfo = filterDataByClass(res);
-    //  console.log(finalInfo);
+    //  const rawForecastDataList = res[1].data.list((listItem) => {
+    //     new forecastWeather(rawForecastData);
+    //    });
+    console.log(finalInfo);
     return finalInfo;
   });
 }
